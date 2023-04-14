398b0dfe18d1a180a999f60f8dc9512badc09b70
Elijah <ilya_prisyazhnyuk@mail.ru>
2023-04-14 14:33:00 +0500

    MathTest added

---
+++ app/src/test/java/com/example/firstproject/MathTest.kt
@@ -0,0 +1,12 @@
+package com.example.firstproject
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MathTest {
+    @Test
+    fun addTest()
+    {
+        assertEquals(2, 1+1)
+    }
+}
